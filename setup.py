0f6ed25924a2fbc9e83fc7e2d1390bf868a4b0aa
Michael Contreras <michael@inetric.com>
2016-06-19 02:46:09 -0400

    mcp: version 0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name='mcp',
   author='Michael Contreras',
-  version='0.0.1',
+  version='0.0.2',
   packages=['mcp'],
   package_dir={'mcp': 'mcp'},
   install_requires=[
